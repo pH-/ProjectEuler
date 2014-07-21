418dc132f45263f8b78772c9fc4104fb4cf79f54
Phanindra Bhagavatula <0x97c8@gmail.com>
2014-07-20 23:49:45 -0400

    latest

--- ProjectEuler/src/com/euler/project/pH/Driver.java
+++ ProjectEuler/src/com/euler/project/pH/Driver.java
@@ -1,8 +1,10 @@
 package com.euler.project.pH;
 
+import java.io.IOException;
+
 public class Driver {
 	
-	public static void main (String[] args)
+	public static void main (String[] args) throws IOException
 	{
 //		Problem1 p1 = new Problem1();
 //		p1.sumOfmultiples();
@@ -14,27 +16,36 @@ public class Driver {
 //		p4.largestPalindrome();
 //		Problem5 p5 = new Problem5();
 //		p5.getMinMultiple();
-		Problem6 p6 = new Problem6();
-		p6.getDiff();
-//		Partition p = new Partition();
-//		int[] input1 = {1,5,11,5};
-//		for(int i=0; i<input1.length; i++)
-//		{
-//			if(input1[i]<=0)
-////				return "Invalid";
-//				System.out.println("Invalid");
-//		}
-//		boolean p1 = p.partitionArray(input1);
-//		
-//		if(p1)
-////			return "Yes";
-//			System.out.println("Yes");
-//		else
-////			return "No";
-//			System.out.println("No");
-//					
-//		System.out.println(p.partitionArray(arr));
-		
+//		Problem6 p6 = new Problem6();
+//		p6.getDiff();
+//		Problem7 p7 = new Problem7();
+//		p7.getnthprime();
+//		Problem8 p8 = new Problem8();
+//		p8.maxProd();
+//		Problem9 p9 = new Problem9();
+//		p9.getPythagoreanTriplet();
+//		Problem10 p10 = new Problem10();
+//		p10.sumOfPrimes();
+//		Problem11 p11 = new Problem11();
+//		p11.getMaxProduct();
+//		Problem12 p12 = new Problem12();
+//		p12.getTriangular();
+//		Problem14 p14 = new Problem14();
+//		p14.getMaxCollatzNum();
+//		Problem16 p16 = new Problem16();
+//		p16.getSum(101);
+//		Problem18 p18 = new Problem18();
+//		p18.getspfValue();
+//		Problem21 p21 = new Problem21();
+//		p21.getAmicableSum();
+//		Problem22 p22 = new Problem22();
+//		p22.getScore();
+//		Problem24 p24 = new Problem24();
+//		p24.getKthLexicographicPerm(999999);
+//		Problem25 p25 = new Problem25();
+//		p25.get100digFibNum();
+		Problem26 p26 = new Problem26();
+		p26.getlongestCycle();
 	}
 
 }
